0a13e1b2c5ca9b7be310f4a6ab6c3c1fb0a2d4d6
Indrajithrnair <indrajithnair02@gmail.com>
2025-06-29 21:27:22 +0530

    Add files via upload

---
+++ UserAccessLogger.java
@@ -0,0 +1,9 @@
+//Task Create a User class with fields marked as private, protected, and public. Use final for a constant. Demonstrate the effects of access //control from another class.
+
+
+class UserAccessLogger{
+
+	private int roll
+
+}
+
