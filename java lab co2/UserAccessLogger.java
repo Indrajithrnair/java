0f4fc76f8f5aa5af276225ec2ea3b11ba42a72ce
Indrajithrnair <indrajithnair02@gmail.com>
2025-10-07 20:29:17 +0530

    Delete java lab co2/UserAccessLogger.java

--- java lab co2/UserAccessLogger.java
+++
@@ -1,9 +0,0 @@
-//Task Create a User class with fields marked as private, protected, and public. Use final for a constant. Demonstrate the effects of access //control from another class.
-
-
-class UserAccessLogger{
-
-	private int roll
-
-}
-
